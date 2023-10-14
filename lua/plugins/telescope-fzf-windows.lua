05488ad23375ac3c20142f940fd6eb2cda694d20
ppoum <pam@poum.ca>
2023-10-14 17:16:22 -0400

    Added telescope-fzf windows build options + updates

---
+++ lua/plugins/telescope-fzf-windows.lua
@@ -0,0 +1,8 @@
+if vim.fn.has("win32") == 1 then
+  return {
+    {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+    },
+  }
+end

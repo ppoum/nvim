c0ac78f8dceb5f826174e8e8179588a5b54a8296
ppoum <pam@poum.ca>
2024-02-08 21:01:20 -0500

    Small changes + updates

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,14 +7,15 @@ vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a T
 vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
 vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
 
--- Only overwrite shell on Windows, leave default on other OSes
+-- Windows-only settings
 if vim.fn.has("win32") == 1 then
-  -- Windows, use powershell instead of cmd
+  -- Use powershell instead of cmd
   vim.cmd([[
     let &shell = executable('pwsh') ? 'pwsh' : 'powershell'
     let &shellcmdflag = '-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues[''Out-File:Encoding'']=''utf8'';Remove-Alias -Force -ErrorAction SilentlyContinue tee;'
     let &shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
     let &shellpipe  = '2>&1 | %%{ "$_" } | tee %s; exit $LastExitCode'
     set shellquote= shellxquote=
-]])
+    ]])
+  vim.g.python3_host_prog = "C:/Python312/python.exe"
 end

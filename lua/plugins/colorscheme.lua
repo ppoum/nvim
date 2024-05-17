04c3926f28afa33c3d8b899e2786637160244e53
Philippe-Alexandre Mathieu <pamathieu@genetec.com>
2024-05-17 09:22:34 -0400

    Update to v0.10, cleanup
    Removed unused Java LS, no longer manually enable inlay hint in
    rustaceanvim, remove legacy comments

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -6,30 +6,9 @@ return {
       colorscheme = "catppuccin-macchiato",
     },
   },
-  -- NOTE: Add old inlay highlighting changes for ease of use for neovim <v0.10
-  -- No longer needed in v0.10 and above (native inlay support)
   {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
-    -- opts = {
-    --   custom_highlights = function(_)
-    --     return {
-    --       LspInlayHint = { fg = "#1c5166" },
-    --     }
-    --   end,
-    -- },
   },
-  -- NOTE: kept for legacy, new lazyvim uses rustacean.nvim
-  -- Change inlay hints highlighting to not be same color as comments
-  -- {
-  --   "simrat39/rust-tools.nvim",
-  --   opts = {
-  --     tools = {
-  --       inlay_hints = {
-  --         highlight = "InlayHint",
-  --       },
-  --     },
-  --   },
-  -- },
 }

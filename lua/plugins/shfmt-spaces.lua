3c92dd0865e52f5f5edee49a65f9716f0ca5a76c
Philippe-Alexandre Mathieu <pamathieu@genetec.com>
2024-05-23 12:56:15 -0400

    Configure shfmt to indent case statements

--- lua/plugins/shfmt-spaces.lua
+++ lua/plugins/shfmt-spaces.lua
@@ -1,10 +1,12 @@
--- Configure conform to add '-i 4' as an arugment to shfmt
+-- Configure conform to add arugments to shfmt
+-- `-i 4`: Indent using 4 spaces
+-- `-ci`: Indent switch cases
 return {
   "stevearc/conform.nvim",
   opts = {
     formatters = {
       shfmt = {
-        prepend_args = { "-i", "4" },
+        prepend_args = { "-i", "4", "-ci" },
       },
     },
   },

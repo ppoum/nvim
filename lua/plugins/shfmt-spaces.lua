aff582d63ef5e5e0d630d6000e6198665e70c4b5
Philippe-Alexandre Mathieu <pamathieu@genetec.com>
2024-05-24 11:07:17 -0400

    feat(shfmt): Keep column padding

--- lua/plugins/shfmt-spaces.lua
+++ lua/plugins/shfmt-spaces.lua
@@ -1,12 +1,13 @@
 -- Configure conform to add arugments to shfmt
 -- `-i 4`: Indent using 4 spaces
 -- `-ci`: Indent switch cases
+-- `-kp`: Keep column padding
 return {
   "stevearc/conform.nvim",
   opts = {
     formatters = {
       shfmt = {
-        prepend_args = { "-i", "4", "-ci" },
+        prepend_args = { "-i", "4", "-ci", "-kp" },
       },
     },
   },
